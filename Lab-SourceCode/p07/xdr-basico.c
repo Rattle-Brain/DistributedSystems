7cf3eb9ad48ff68109d68c7d160bd1ce7b8b23a6
Rattle-Brain <danialgo2001@gmail.com>
2023-03-13 18:59:36 +0100

    modified:   xdr-basico.c

--- Lab-SourceCode/p07/xdr-basico.c
+++ Lab-SourceCode/p07/xdr-basico.c
@@ -1,8 +1,9 @@
 #include <stdio.h> // Para FILE*
 #include <errno.h> // para perror()
 #include <stdlib.h> // para exit()
-#include <rpc/types.h>
-#include <rpc/xdr.h>
+#include <tirpc/rpc/types.h>
+#include <tirpc/rpc/rpc.h>
+#include <tirpc/rpc/xdr.h>
 
 #define DEFAULT_INTEGER_VALUE 100
 #define DEFAULT_CHAR_VALUE 'A'
@@ -18,23 +19,26 @@ int main(){
 		perror("Error al abrir el fichero");
 		exit(1);
 	}
+	
 	// Init XDR filter
-	// xdrstdio_create(&operacion, fich, XDR_ENCODE);
+	xdrstdio_create(&operacion, fich, XDR_ENCODE);
 	
 	// Ask the user for values
 	printf("Enter an integer:\n");
 	scanf("%d", &n);
+	
 	if (n < 0){
 		perror("The value is not valid, setting default");
 		n = DEFAULT_INTEGER_VALUE;
 	}
+	
 	printf("Enter a char:\n");
 	scanf("%s", &cad);
+	
 	if(sizeof(char)/sizeof(cad) != 1){
 		perror("CHAR cannot be null or longer than 1, setting default");
 		cad = DEFAULT_CHAR_VALUE;
 	}
-
 
 	// Store Values into file
 	xdr_int(&operacion, &n);
