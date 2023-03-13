6db67c5f7f239ea2ecae29b7e84e62b85732b921
Rattle-Brain <danialgo2001@gmail.com>
2023-03-13 18:29:20 +0100

    Created EXEC again

--- Lab-SourceCode/p07/xdr-basico.c
+++ Lab-SourceCode/p07/xdr-basico.c
@@ -1,12 +1,15 @@
 #include <stdio.h> // Para FILE*
 #include <errno.h> // para perror()
 #include <stdlib.h> // para exit()
-#include <sys/types.h>
-#include <rpc/rpc.h>
+#include <rpc/types.h>
+#include <rpc/xdr.h>
+
+#define DEFAULT_INTEGER_VALUE 100
+#define DEFAULT_CHAR_VALUE 'A'
 
 int main(){
-	int n = 100;
-	char cad = 'F';
+	int n;
+	char cad; 
 	FILE* fich;
 	XDR operacion;
 
@@ -15,9 +18,29 @@ int main(){
 		perror("Error al abrir el fichero");
 		exit(1);
 	}
-	xdrstdio_create(&operacion, fich, XDR_ENCODE);
+	// Init XDR filter
+	// xdrstdio_create(&operacion, fich, XDR_ENCODE);
+	
+	// Ask the user for values
+	printf("Enter an integer:\n");
+	scanf("%d", &n);
+	if (n < 0){
+		perror("The value is not valid, setting default");
+		n = DEFAULT_INTEGER_VALUE;
+	}
+	printf("Enter a char:\n");
+	scanf("%s", &cad);
+	if(sizeof(char)/sizeof(cad) != 1){
+		perror("CHAR cannot be null or longer than 1, setting default");
+		cad = DEFAULT_CHAR_VALUE;
+	}
+
+
+	// Store Values into file
 	xdr_int(&operacion, &n);
 	xdr_char(&operacion, &cad);
+
+	// Destroy and close to exit program
 	xdr_destroy(&operacion);
 	fclose(fich);
 	return 0;
